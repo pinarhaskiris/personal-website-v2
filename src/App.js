3d1af256a2eb9824a3405bdc0626cd9f050b2559
pinarhaskiris <pinarhaskiris1900@gmail.com>
2022-04-06 01:55:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import Navbar from "./components/Navbar";
 import Project from "./components/Project";
 import ArchiveItem from "./components/ArchiveItem";
-import SimpleSlider from "./components/SimpleSlider";
+import Carousel from "./components/Carousel";
 
 import annie from "./images/annie.png";
 import email_client from "./images/email_client.png";
@@ -37,8 +37,8 @@ function App() {
         <div>
         <h1>Pınar Haskırış</h1>
           <ul>
-            <li>👩🏻‍💻 A 21 year-old computer engineering student from Istanbul. </li>
-            <li>☺️ Interning at <a href="https://sociality.io" target="_blank">Sociality.io</a> as a front-end developer since August, 2021.</li>
+            <li>👩🏻‍💻 A 21-year-old computer engineering student from Istanbul. </li>
+            <li>☺️ Interning at <a href="https://sociality.io" target="_blank">Sociality.io</a> as a front-end developer since August 2021.</li>
             <li>🧠 Currently working on using deep learning models for hate-speech recognition for my senior design project.</li>
             <li>🎨 Interested in art and languages outside of engineering.</li>
           </ul>
@@ -52,10 +52,10 @@ function App() {
         
           <Project img={ senior_design_project }
             title="Senior Design Project I"
-            description={["Three machine learning algorithms (Naive Bayes, SVM, Random Forest) was used in hate-speech recognition.", 
+            description={["Three machine learning algorithms (Naive Bayes, SVM, Random Forest) were used in hate-speech recognition.", 
             "90% accuracy was obtained with SVM.",
             "Steps included preparing the data, training the models and evaluating their results.",
-            "Second part of the project aims to use deep learning models to achieve higher accuracy."]}
+            "The second part of the project aims to use deep learning models to achieve higher accuracy."]}
             tags={["Python", "Machine Learning", "Classification"]}
             githubLink="https://github.com/pinarhaskiris/hate-speech-recognizer"
           />
@@ -63,7 +63,7 @@ function App() {
           <Project img={ annie }
             title="Annie"
             description={["A front-end Named-Entity annotation tool. Build for annotating with Person, Location and Organization tags.", 
-            "Users can import the text as .txt file, annotate using the tool and export the annotations with BIO tags included."]}
+            "Users can import the text as a .txt file, annotate using the tool and export the annotations with BIO tags included."]}
             tags={["HTML", "CSS", "React.js"]}
             githubLink="https://github.com/pinarhaskiris/annie"
             liveLink="https://pinarhaskiris.github.io/annie/"/>
@@ -82,11 +82,12 @@ function App() {
           <h1>Other Notable Projects</h1>
           <div className="archive_list">
             <ArchiveItem title="cute-dark" description="A dark theme for VS Code. Made purely for fun."
+            tags={["VS Code", "Extension"]}
             githubLink="https://github.com/pinarhaskiris/cute-dark"
             liveLink="https://marketplace.visualstudio.com/items?itemName=pinarhaskiris.cute-dark"/>
 
             <ArchiveItem title="Text Decryptor" 
-            description="The grammar and regex rules used to generate and encrypt the text is known. The program decryptes the file by applying regex rules reversely and removing the sentences that can not be generated with the given grammar rules."
+            description="The grammar and regex rules used to generate and encrypt the text is known. The program decrypts the file by applying regex rules reversely and removing the sentences that can not be generated with the given grammar rules."
             tags={["Java", "Automata Theory and Formal Languages"]} githubLink="https://github.com/pinarhaskiris/COMP321-Assignment"/>
 
             <ArchiveItem title="Number Recognizer" description="This program takes an image as input, draws red rectangles around the numbers it recognizes and writes the recognized number on top of it. OpenCV was not used, calculations were done manually."
@@ -110,8 +111,8 @@ function App() {
         <div>
           <img src={resume_ss} />
           <div className="links">
-            <a href="/files/Resume.pdf" target="_blank">Open in new tab <BsBoxArrowUpRight style={{ paddingLeft: '10px' }} /></a>
-            <a href="/files/Resume.pdf" target="_blank" download>Download in PDF <BsDownload style={{ paddingLeft: '10px' }} /></a>
+            <a href="/files/Pınar_Haskırış_Resume.pdf" target="_blank">Open in new tab <BsBoxArrowUpRight style={{ paddingLeft: '10px' }} /></a>
+            <a href="/files/Pınar_Haskırış_Resume.pdf" target="_blank" download>Download in PDF <BsDownload style={{ paddingLeft: '10px' }} /></a>
           </div>
         </div>
       </div>
@@ -119,10 +120,10 @@ function App() {
       <div id="art_container">
         <h1>Art</h1>
         <div>
-          <SimpleSlider />
+          <Carousel />
         </div>
 
-        <a href="/files/thoughts.pdf" target="_blank">for more <BsBoxArrowUpRight style={{ paddingLeft: '10px' }} /> </a>
+        <a href="https://drive.google.com/file/d/1EDO9VVazz2vOgzmz1QzNl2HBkaaArls1/view?usp=sharing" target="_blank">for more <BsBoxArrowUpRight style={{ paddingLeft: '10px' }} /> </a>
         
       </div>
 
